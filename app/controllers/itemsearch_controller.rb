d0c7419ca8b36f40b01e70fbefe6f2b8bba04fc7
Yosuke OTA <yota@ns.ie.u-ryukyu.ac.jp>
2013-09-05 14:23:26 +0900

    modify api keys

--- app/controllers/itemsearch_controller.rb
+++ app/controllers/itemsearch_controller.rb
@@ -12,8 +12,8 @@ class ItemsearchController < ApplicationController
 
     begin
       data = httpClient.get_content('https://app.rakuten.co.jp/services/api/IchibaItem/Search/20130805', {
-          'applicationId' => '1025899060325418115',
-          'affiliateId'   => '0dd78f8a.26c4c8c0.0dd78f8b.0ee811d2',
+          'applicationId' => '1098947557275482302',
+          'affiliateId'   => '11b23d62.426bf5af.11b23d63.461ea0d8',
           'keyword'       => keyword
       })
       @jsonData = JSON.parse data
